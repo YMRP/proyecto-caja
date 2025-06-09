9c0c659d87b404922eaf64d6cce321c2d256d3c9
Yostin Manuel Ramos Pinto <ramosyostin2003@gmail.com>
2025-06-09 12:37:09 -0600

    commit de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>HOLA MUNDO</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
